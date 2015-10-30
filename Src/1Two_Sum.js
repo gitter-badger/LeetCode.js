4cb802b56d3d2c43aa5153de5cf04e2a4a108153
CyanMac <U201315021@hust.edu.cn>
2015-10-30 21:44:25 +0800

    Implement Q1

---
+++ Src/1Two_Sum.js
@@ -0,0 +1,33 @@
+/*
+ * Given an array of integers,
+ * find two numbers such that they add up to a specific target number.
+ *
+ * The function twoSum should return indices of the two numbers
+ * such that they add up to the target, where index1 must be less than index2.
+ * Please note that your returned answers (both index1 and index2) are not zero-based.
+ *
+ * You may assume that each input would have exactly one solution.
+ *
+ * Input: numbers={2, 7, 11, 15}, target=9
+ * Output: index1=1, index2=2
+ */
+
+ /**
+  * @param {number[]} nums
+  * @param {number} target
+  * @return {number[]}
+  */
+var twoSum = function(nums, target) {
+    var i,
+        j,
+        len = nums.length;
+    for(i = 0; i < len; i++) {
+        for(j = i + 1; j < len; j++) {
+            if(nums[i] + nums[j] === target) {
+                return [i + 1, j + 1];
+            }
+        }
+    }
+};
+
+exports.twoSum = twoSum;
---
+++ Test/1Two_Sum_Test.js
@@ -0,0 +1,12 @@
+var two_sum = require('../Src/1Two_Sum').twoSum;
+
+var Test = function (cb) {
+    var case1 = [1, 2, 3, 4, 5];
+
+    var reulst = cb(case1, 9);
+    if(reulst[0] === 4 && reulst[1] === 5) {
+        console.log('Test Ok!');
+    }
+};
+
+Test(two_sum);
