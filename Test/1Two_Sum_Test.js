e3a0ea379832da10d8ce7d8c52e4fa302a3e89f5
CyanMac <U201315021@hust.edu.cn>
2015-11-01 09:07:54 +0800

    add more test case

--- Test/1Two_Sum_Test.js
+++ Test/1Two_Sum_Test.js
@@ -2,10 +2,13 @@ var two_sum = require('../Src/1Two_Sum').twoSum;
 
 var Test = function (cb) {
     var case1 = [1, 2, 3, 4, 5];
+    var case2 = [4, 6, 8, 9, 0];
 
-    var reulst = cb(case1, 9);
-    if(reulst[0] !== 4 && reulst[1] !== 5) {
+    if(cb(case1, 9)[0] !== 4 && cb(case1, 9)[1] !== 5) {
         console.log('Test Error!');
+    }
+    if(cb(case2, 17)[0] !== 3 && cb(case1, 9)[1] !== 4) {
+        console.log('Test Error');
     }
 };
 
