bffe1ee9c6f9765ba8b92fb999b740cc1b44fcb7
ZhiweiSun <U201315021@hust.edu.cn>
2015-11-07 14:16:35 +0800

    Implemrnt problem 2: add two number
    but not add test case

--- Src/2Add_Two_Numbers.js
+++ Src/2Add_Two_Numbers.js
@@ -7,31 +7,41 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 */
 
+
+// Definition for singly-linked list.
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-    var bit = 1;
-    var sum = 0;
-    var Node;
-    while(l1 !== null || l2 !== null) {
-        if(l1 !== null) {
-            sum += l1.val * bit;
-            l1 = l1.next;
-        }
-        if(l2 !== null) {
-            sum += l2.val * bit;
-            l2 = l2.next;
-        }
-        bit *= 10;
-    }
-    do{
-        Node = ListNode(sum % 10);
-        sum /= 10;
-        Node = Node.next;
-    }while(sum % 10);
+ var addTwoNumbers = function(l1, l2) {
+     var bit = 1;
+     var sum = 0;
+     var Node;
+     var tmp;
+     while(l1 !== null || l2 !== null) {
+         if(l1 !== null) {
+             sum += l1.val * bit;
+             l1 = l1.next;
+         }
+         if(l2 !== null) {
+             sum += l2.val * bit;
+             l2 = l2.next;
+         }
+         bit *= 10;
+     }
+     tmp = Node = new ListNode(sum % 10);
+     sum = parseInt(sum / 10);
+     while(sum !== 0){
+         var temp = new ListNode(sum % 10);
+         tmp = tmp.next = temp;
+         sum = parseInt(sum / 10);
+     }
 
-    return Node;
-};
+     return Node;
+ };
