7fecb792f1959420f5ea48eb336096eeb20b3b5b
ZhiweiSun <U201315021@hust.edu.cn>
2015-11-10 18:27:48 +0800

    add problem 7 Reverse Integer

---
+++ Test/7Reverse_Integer_Test.js
@@ -0,0 +1,7 @@
+var a = 123;
+a = a.toString();
+var elem;
+console.log(typeof a);
+for (elem in a) {
+    console.log(a);
+}
--- Test/LeetCodetest.js
+++ Test/LeetCodetest.js
@@ -1,3 +1,4 @@
 require('./1Two_Sum_Test');
 require('./2Add_Two_Number_Test');
-require('./3Longest_Substring_Without_Repeating_Characters_Test');
+//require('./3Longest_Substring_Without_Repeating_Characters_Test');
+require('./7Reverse_Integer_Test');
