d98c34fcc2e752c6bf36ec6ca3760f745d5c920b
ZhiweiSun <U201315021@hust.edu.cn>
2015-11-11 14:36:06 +0800

    add problem 9 Palindrome Number

--- Test/LeetCodetest.js
+++ Test/LeetCodetest.js
@@ -1,4 +1,5 @@
 require('./1Two_Sum_Test');
 require('./2Add_Two_Number_Test');
-require('./3Longest_Substring_Without_Repeating_Characters_Test');
+//require('./3Longest_Substring_Without_Repeating_Characters_Test');
 require('./7Reverse_Integer_Test');
+require('./9Palindrome_Number_Test');
