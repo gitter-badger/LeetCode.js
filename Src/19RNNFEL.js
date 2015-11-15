fa037872422694623c5c01321fc1c882bd95328b
ZhiweiSun <U201315021@hust.edu.cn>
2015-11-15 22:31:23 +0800

    Implement problem 19

--- Src/19RNNFEL.js
+++ Src/19RNNFEL.js
@@ -13,3 +13,57 @@
  * Given n will always be valid.
  * Try to do this in one pass.
  */
+
+/**
+ * Definition for singly-linked list.
+ *
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+ 
+ /**
+  * @param {ListNode} head
+  * @param {number} n
+  * @return {ListNode}
+  */
+var removeNthFromEnd = function(head, n) {
+    var len = ListNodeCount(head);
+    var goal = len - n + 1;
+    var count = 1;
+    var temp = head;
+    if(goal === 1) {
+        return head.next;
+    }
+    else {
+        while(temp !== null) {
+            count++;
+            if(count === goal) {
+                if(goal === len) {
+                    temp.next = null;
+                    return head;
+                }
+                temp.next = temp.next.next;
+                return head;
+            }
+            temp = temp.next;
+        }
+    }
+};
+
+/**
+ * @description return the length of a singly-linked list
+ * @param {ListNode} head
+ * @return {number}
+ */
+var ListNodeCount = function(head) {
+    var count = 0;
+    while(head !== null) {
+        count++;
+        head = head.next;
+    }
+    return count;
+};
+
+exports.removeNthFromEnd = removeNthFromEnd;
