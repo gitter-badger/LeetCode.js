73febdd8e828afb235538ec0cebd66c165124da7
ZhiweiSun <U201315021@hust.edu.cn>
2015-11-16 17:29:08 +0800

    Implement Problem 21 Merge Two Sorted Lists

--- Src/21Merge_Two_Sorted_Lists.js
+++ Src/21Merge_Two_Sorted_Lists.js
@@ -5,12 +5,13 @@
  */
 
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
-*/
+ * @description Definition for singly-linked list.
+ */
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
 
 /**
  * @param {ListNode} l1
@@ -18,5 +19,31 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-
+    var temp = new ListNode(0);
+    var head = temp;
+    while(l1 !== null && l2 !== null) {
+        if(l1.val < l2.val) {
+            temp = temp.next = new ListNode(l1.val);
+            l1 = l1.next;
+        }
+        else {
+            temp = temp.next = new ListNode(l2.val);
+            l2 = l2.next;
+        }
+    }
+    if(l1 === null) {
+        while(l2 !== null) {
+            temp = temp.next = new ListNode(l2.val);
+            l2 = l2.next;
+        }
+    }
+    if(l2 === null) {
+        while(l1 !== null) {
+            temp = temp.next = new ListNode(l1.val);
+            l1 = l1.next;
+        }
+    }
+    return head.next;
 };
+
+exports.mergeTwoLists = mergeTwoLists;
