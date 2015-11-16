37e16c3bc7504525fed72cd8a704d194300c26cd
ZhiweiSun <U201315021@hust.edu.cn>
2015-11-16 23:17:22 +0800

    add gulp plugin gulp-git

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,7 @@ var jshint = require('gulp-jshint');
 var nodemon = require('gulp-nodemon');
 var fs = require('fs');
 var watch = require('gulp-watch');
+var git = require('gulp-git');
 
 gulp.task('jshint', function () {
     gulp.src('./Src/*.js')
@@ -14,10 +15,9 @@ gulp.task('jshint', function () {
         .pipe(jshint.reporter('default'));
 });
 
-gulp.task('node', function () {
+gulp.task('node',['jshint'], function () {
     nodemon({
-        script: './Test/LeetCodetest.js',
-        task: ['jshint']
+        script: './Test/LeetCodetest.js'
     });
 });
 
@@ -37,6 +37,16 @@ gulp.task('update_percentage', function () {
         var newdata = data.replace(/\!\[Progress\].*\)/, url);
         fs.writeFile('./README.md', newdata, 'utf-8');
     });
+});
+
+gulp.task('add', function(){
+    gulp.src('./README.md')
+        .pipe(git.add());
+});
+
+gulp.task('commit', ['update_percentage', 'add'], function() {
+    return gulp.src('./README.md')
+        .pipe(git.commit('update percentage'));
 });
 
 gulp.task('watch', function() {
