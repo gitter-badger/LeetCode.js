cbfa31d94bcb5d3c217d8376416657a23260752f
ZhiweiSun <U201315021@hust.edu.cn>
2015-11-16 15:19:08 +0800

    add peoblrm 20 Valid Parentheses

---
+++ Src/20Valid_Parentheses.js
@@ -0,0 +1,16 @@
+/**
+ * Given a string containing just the characters '(', ')', '{', '}'
+ * and '[' and ']', determine if the input string is valid.
+ *
+ * The brackets must close in the correct order, "()" and "()[]{}"
+ * are all valid but "(]" and "([)]" are not.
+ */
+
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+
+};
