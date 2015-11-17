2cca78684be8fa1c584b29f57a42aca35d64a183
ZhiweiSun <U201315021@hust.edu.cn>
2015-11-17 23:19:30 +0800

    add problem 225 Implement Stack using Queues

---
+++ Src/225Implement_Stack_using_Queues.js
@@ -0,0 +1,54 @@
+/**
+ * Implement the following operations of a stack using queues.
+ *
+ * @function push(x) -- Push element x onto stack.
+ * @function pop() -- Removes the element on top of the stack.
+ * @function top() -- Get the top element.
+ * @function empty() -- Return whether the stack is empty.
+ * Notes:
+ * You must use only standard operations of a queue,
+ * which means only push to back, peek/pop from front, size,
+ * and is empty operations are valid.
+ *
+ * Depending on your language, queue may not be supported natively.
+ * You may simulate a queue by using a list or deque (double-ended queue),
+ * as long as you use only standard operations of a queue.
+ *
+ * You may assume that all operations are valid (for example,
+ * no pop or top operations will be called on an empty stack).
+
+/**
+ * @constructor
+ */
+var Stack = function() {
+
+};
+
+/**
+ * @param {number} x
+ * @returns {void}
+ */
+Stack.prototype.push = function(x) {
+
+};
+
+/**
+ * @returns {void}
+ */
+Stack.prototype.pop = function() {
+
+};
+
+/**
+ * @returns {number}
+ */
+Stack.prototype.top = function() {
+
+};
+
+/**
+ * @returns {boolean}
+ */
+Stack.prototype.empty = function() {
+
+};
