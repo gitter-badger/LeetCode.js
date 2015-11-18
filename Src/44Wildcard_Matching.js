be29e94f3b3a2e99e23e7f3e5cd86e70e454336a
ZhiweiSun <U201315021@hust.edu.cn>
2015-11-19 00:11:40 +0800

    Add problem 44 Wildcard Matching

---
+++ Src/44Wildcard_Matching.js
@@ -0,0 +1,32 @@
+/**
+ * Implement wildcard pattern matching with support for '?' and '*'.
+ *
+ * @see
+ * '?' Matches any single character.
+ * '*' Matches any sequence of characters (including the empty sequence).
+ *
+ * The matching should cover the entire input string (not partial).
+ *
+ * The function prototype should be:
+ * bool isMatch(const char *s, const char *p)
+ *
+ * @example
+ * Some examples:
+ * isMatch("aa","a") → false
+ * isMatch("aa","aa") → true
+ * isMatch("aaa","aa") → false
+ * isMatch("aa", "*") → true
+ * isMatch("aa", "a*") → true
+ * isMatch("ab", "?*") → true
+ * isMatch("aab", "c*a*b") → false
+ */
+
+/**
+ * @TODO Implement Wildcard Matching 
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) {
+
+};
