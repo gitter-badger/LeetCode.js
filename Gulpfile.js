a3ab597049644c5772f97f1c633b15f8adf3dbe7
ZhiweiSun <U201315021@hust.edu.cn>
2015-11-18 23:29:05 +0800

    Changes to be committed:
    modified:   Gulpfile.js
    modified:   package.json

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var nodemon = require('gulp-nodemon');
 var fs = require('fs');
-var watch = require('gulp-watch');
 var git = require('gulp-git');
 var shell = require('gulp-shell');
 var yargs = require('yargs').argv;
@@ -12,11 +11,7 @@ gulp.task('count', shell.task(
 ));
 
 gulp.task('jshint', function () {
-    gulp.src('./Src/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-
-    gulp.src('./Test/*.js')
+    gulp.src(['./Src/*.js', './Test/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
@@ -27,31 +22,26 @@ gulp.task('node',['jshint'], function () {
     });
 });
 
-
 gulp.task('update_percentage', function () {
     fs.readFile('./README.md', "utf-8", function(err, data) {
-        var completed = "[x] " + yargs.n;
+        var to_add = "[x] " + yargs.n;
         var Reg = new RegExp('\\[ \\] '+yargs.n);
-        var do_data = data.replace(Reg, completed);
+        var do_data = data.replace(Reg, to_add);
 
         var total = do_data.match(/- \[[x ]\]/gi).length;
         var completed = do_data.match(/\[x\]/gi).length;
         var percent = (completed / total * 100).toFixed(2);
-        var url = "![Progress](https://img.shields.io/badge/Progress-"
-                    + completed
-                    + "%20%2F%20"
-                    + total
-                    + "%20%3D%20"
-                    + percent
-                    + "%25-green.svg)";
+        var url = "![Progress](https://img.shields.io/badge/Progress-" +
+                    completed +
+                    "%20%2F%20"+
+                    total+
+                    "%20%3D%20"+
+                    percent+
+                    "%25-green.svg)";
+        console.log(url);
         var newdata = do_data.replace(/\!\[Progress\].*\)/, url);
         fs.writeFile('./README.md', newdata, 'utf-8');
     });
-});
-
-gulp.task('add', function(){
-    gulp.src('./README.md')
-        .pipe(git.add());
 });
 
 gulp.task('commit', ['update_percentage'], function() {
