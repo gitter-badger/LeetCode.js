49f92c9e9af8bd76350eb1ffeaf0cd863b761d68
ZhiweiSun <U201315021@hust.edu.cn>
2015-11-19 14:21:51 +0800

    Add problem 295 Find Median from Data Stream

---
+++ Src/295Find_Median_from_Data_Stream.js
@@ -0,0 +1,56 @@
+/**
+ * Median is the middle value in an ordered integer list.
+ * If the size of the list is even, there is no middle value.
+ * So the median is the mean of the two middle value.
+ *
+ * @example
+ * Examples:
+ * [2,3,4] , the median is 3
+ *
+ * [2,3], the median is (2 + 3) / 2 = 2.5
+ *
+ * Design a data structure that supports the following two operations:
+ *
+ * @function void addNum(int num)
+ *      - Add a integer number from the data stream to the data structure.
+ * @function double findMedian()
+ *      - Return the median of all elements so far.
+ * @see
+ * For example:
+ *
+ * add(1)
+ * add(2)
+ * findMedian() -> 1.5
+ * add(3)
+ */
+
+/**
+ * @constructor
+ */
+var MedianFinder = function() {
+
+};
+
+/**
+ * @description Adds a num into the data structure.
+ * @param {integer} word
+ * @return {void}
+ */
+MedianFinder.prototype.addNum = function(num) {
+
+};
+
+/**
+ * @description Return median of current data stream
+ * @return {double}
+ */
+MedianFinder.prototype.findMedian = function() {
+
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * var mf = new MedianFinder();
+ * mf.addNum(1);
+ * mf.findMedian();
+ */
