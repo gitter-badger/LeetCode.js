85d6a761a2b10aca16bb557bfd5b3bc7461f12e1
ZhiweiSun <U201315021@hust.edu.cn>
2015-11-20 13:26:18 +0800

    Add problem 307 Range Sum Query - Mutable

---
+++ Src/307Range_Sum_Query-Mutable.js
@@ -0,0 +1,53 @@
+/**
+ * Given an integer array nums,
+ * find the sum of the elements between indices i and j (i ≤ j), inclusive.
+ *
+ * @function update(i, val)
+ *          - modifies nums by updating the element at index i to val.
+ * @example:
+ * Given nums = [1, 3, 5]
+ *
+ * sumRange(0, 2) -> 9
+ * update(1, 2)
+ * sumRange(0, 2) -> 8
+ * @see
+ * Note:
+ * The array is only modifiable by the update function.
+ * You may assume the number of calls to update
+ * and sumRange function is distributed evenly.
+ */
+
+/**
+ * @constructor
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+
+};
+
+/**
+ * @param {number} i
+ * @param {number} val
+ * @return {void}
+ */
+NumArray.prototype.update = function(i, val) {
+
+};
+
+/**
+ * @param {number} i
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+
+};
+
+
+/**
+  * Your NumArray object will be instantiated and called as such:
+  * var numArray = new NumArray(nums);
+  * numArray.sumRange(0, 1);
+  * numArray.update(1, 10);
+  * numArray.sumRange(0, 2);
+  */
