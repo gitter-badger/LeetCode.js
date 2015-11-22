aa9ecddd97ef0dbbe30e57c010f8884196d19b5e
ZhiweiSun <U201315021@hust.edu.cn>
2015-11-22 22:51:11 +0800

    Add problem 191 Number of 1 Bits

---
+++ Src/191Number_of_1_Bits.js
@@ -0,0 +1,26 @@
+/**
+ * Write a function that takes an unsigned integer
+ * and returns the number of ’1' bits it has (also known as the Hamming weight).
+ *
+ * For example, the 32-bit integer ’11'
+ * has binary representation 00000000000000000000000000001011,
+ * so the function should return 3.
+ *
+ * Credits:
+ * Special thanks to @ts for adding this problem and creating all test cases.
+ *
+ * Subscribe to see which companies asked this question
+ */
+
+
+/**
+ * @TODO Implement Number of 1 Bits 
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+
+};
+
+
+exports.hammingWeight = hammingWeight;
